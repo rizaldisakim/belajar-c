809c9925ed9ead9142651b3ba621022fab4baf09
Rizaldi Sakim Suhadi <konkaleekong@gmail.com>
2026-01-18 18:00:05 +0300

    Belajar C lagi

--- youtube.c
+++ youtube.c
@@ -4,8 +4,11 @@ int main() {
     int year = 2026;
     int project = 3;
     double harga_pc = 1500.30;
+    char k = 'b';
 
     printf("This year of %d will be the greatest year of my life!\n", year);
     printf("I'll make more than %d projects this year!\n", project);
     printf("Dan beli sepatu Air Jordan baru seharga $%lf\n", harga_pc);
+    printf("%c", k);
+
 }
